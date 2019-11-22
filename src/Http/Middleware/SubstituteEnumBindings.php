84b0cdc5bf6126770b318c640ca289ab099f001d
Bert van Hoekelen <bert@magwel.nl>
2019-11-22 16:30:36 +0100

    Add enum binding middleware

---
+++ src/Http/Middleware/SubstituteEnumBindings.php
@@ -0,0 +1,32 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Container\Container;
+use Illuminate\Contracts\Routing\Registrar;
+use LenderSpender\LaravelEnums\Routing\ImplicitEnumRouteBinding;
+
+class SubstituteEnumBindings
+{
+    /** @var \Illuminate\Contracts\Routing\Registrar */
+    private $router;
+
+    /** @var \Illuminate\Container\Container */
+    private $container;
+
+    public function __construct(Registrar $router, Container $container)
+    {
+        $this->router = $router;
+        $this->container = $container;
+    }
+
+    public function handle($request, Closure $next)
+    {
+        ImplicitEnumRouteBinding::resolveForRoute($this->container, $request->route());
+
+        return $next($request);
+    }
+}
